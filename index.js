1142d279098dcc1b202d8f02505b0fee79c9ae53
AniekanabasiAbasiurua <aniekanabasi.abasiurua@gmail.com>
2024-01-21 12:05:42 -0500

    phase-0-intro-to-js-2-looping-code-along complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+// writeCards(["Charlie", "Samip", "Ali"], "wedding")
+
+function writeCards(arr, event) {
+    const newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        newArr.push(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`);
+    }
+    return newArr;
+}
+
+function countDown(posInt) {
+    while (posInt >= 0) {
+        console.log(posInt);
+        posInt--;
+    }
+}
